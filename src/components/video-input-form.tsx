75758de76945e2d85383ba9b5315b010db163715
LucasCota <lucas.a.cota@gmail.com>
2023-09-15 14:44:32 -0300

    convert-video-to-audio

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -2,13 +2,14 @@ import { FileVideo, Upload } from "lucide-react";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ChangeEvent, useMemo, useState } from "react";
-
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from '@ffmpeg/util'
 export function VideoInputForm() {
     const [videoFile, setVideoFile] = useState<File | null>(null)
+    const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
-
-    function handleFileSelector(event: ChangeEvent<HTMLInputElement>) {
+    function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
         const { files } = event.currentTarget
 
         if(!files) {
@@ -21,6 +22,62 @@ export function VideoInputForm() {
 
     }
 
+
+    async function convertVideoToAudio(video: File){
+        console.log('Convert started.')
+
+        const ffmpeg = await getFFmpeg() 
+
+        await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+        /* ffmpeg.on('log', log => {
+            console.log('log')
+        }) */
+
+        ffmpeg.on('progress', progress => {
+            console.log('Convert progress: ' + Math.round(progress.progress * 100))
+        })
+
+        await ffmpeg.exec([
+            '-i',
+            'input.mp4',
+            '-map',
+            '0:a',
+            '-b:a',
+            '20k',
+            '-acodec',
+            'libmp3lame',
+            'output.mp3'
+        ])
+
+        const data = await ffmpeg.readFile('output.mp3')
+
+        const audioFileBlob = new Blob([data], {type: 'audio/mpeg'})
+        const audioFile = new File([audioFileBlob], 'audio.mp3', {
+            type: 'audio/mpeg'
+        })
+
+        console.log('Convert finished.')
+
+        return audioFile
+    }
+
+    async function handleUploadVideo(event: FormEvent<HTMLFormElement>){
+        event.preventDefault()
+
+        const prompt = promptInputRef.current?.value
+
+        if(!videoFile){
+            return
+        }
+
+        //converter o video em audio
+        
+        const audioFile = await convertVideoToAudio(videoFile)
+
+        console.log(audioFile)
+    }
+
     const previewUrl = useMemo(() => {
         if(!videoFile) {
             return null
@@ -31,7 +88,7 @@ export function VideoInputForm() {
 
     return (
         <>
-         <form className="space-y-4">
+         <form onSubmit={handleUploadVideo} className="space-y-4">
             <label 
               htmlFor="video" 
               className="relative border flex rounded-md aspect-[6/2] cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
@@ -47,7 +104,7 @@ export function VideoInputForm() {
                )}
             </label>
 
-            <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelector}/>
+            <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected}/>
 
             
 
@@ -56,6 +113,7 @@ export function VideoInputForm() {
                 Prompt de transcrição
               </Label>
               <Textarea 
+                ref={promptInputRef}
                 id="transcription_prompt" 
                 className="leading-relaxed resize-none"
                 placeholder="Incluas palavras-chave mencionadas no vídeo separadas por vírgula"
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
